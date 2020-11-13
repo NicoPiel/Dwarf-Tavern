1025648a861b7229b86fc2d2f64786b59d07ac46
Pascal <pacolos@web.de>
2020-11-13 11:07:11 +0100

    Added Default GameManager.cs
    With singleton pattern

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        _instance = this;
+    }
+
+    public static GameManager GetGameManager()
+    {
+        return _instance;
+    }
+}
