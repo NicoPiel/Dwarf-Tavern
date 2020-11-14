6bc32f5d4f1ad8af39b704773443d2b12ae5e614
Mark B <mark.buchalski@gmail.com>
2020-11-14 17:40:26 +0100

    Added interaction system

---
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    private bool _cancelInteraction;
+    protected abstract void OnInteract();
+
+    public void SetCancelled(bool cancelled)
+    {
+        _cancelInteraction = cancelled;
+    }
+
+    public void Interact()
+    {
+        SetCancelled(false);
+        GameManager.GetEventHandler().onPlayerInteract.Invoke(this);
+        if(!_cancelInteraction)
+            OnInteract();
+    }
+}
