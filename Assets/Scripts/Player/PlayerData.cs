8203bedd5ac84ee94067782493ef8f4d51eb0b02
Pascal <pacolos@web.de>
2020-11-18 17:06:02 +0100

    Added Test Assets and Test Scene

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -7,7 +7,7 @@ namespace Player
         // Start is called before the first frame update
         void Start()
         {
-        
+            
         }
 
         // Update is called once per frame
