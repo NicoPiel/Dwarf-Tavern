b7299c03485844d913b4762e9676710b8229692f
Caigh <nico.piel@hotmail.de>
2020-11-19 16:14:20 +0100

    Added player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float playerSpeed;
+        private Vector3 move;
+        private CharacterController characterController;
+
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-        
+            characterController = gameObject.GetComponent<CharacterController>();
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            characterController.Move(move * (Time.fixedDeltaTime * playerSpeed));
         }
     }
 }
