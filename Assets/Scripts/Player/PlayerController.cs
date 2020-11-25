0f0988df99a3f22f3411c74dfce3a9b8a666c2a8
Mark B <mark.buchalski@gmail.com>
2020-11-25 17:40:52 +0100

    Smooth movement testfeature

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,8 +7,11 @@ namespace Player
     {
         [SerializeField] private float playerSpeed;
         private Vector2 _move;
+        private Vector2 _previous;
         private Rigidbody2D _rigidbody;
         private Collider2D _collider;
+        public bool smoothMovement;
+        public int smoothness = 5;
 
         // Start is called before the first frame update
         private void Start()
@@ -27,6 +30,12 @@ namespace Player
 
         private void FixedUpdate()
         {
+            if (smoothMovement)
+            { 
+                _move.x = ((_previous.x * (smoothness - 1)) + _move.x) / smoothness;
+                _move.y = ((_previous.y * (smoothness - 1)) + _move.y) / smoothness;
+                _previous = new Vector2(_move.x, _move.y);
+            }
             _rigidbody.MovePosition(_rigidbody.position + _move * (playerSpeed * Time.fixedDeltaTime));
         }
     }
